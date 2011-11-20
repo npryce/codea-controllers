b1a69a0abce0615964698fe84d6a0ec392447a8a
Nat Pryce <github@natpryce.com>
2011-11-20 22:37:37 +0000

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,20 +1,23 @@
+-- Moving a sprite with a VirtualStick
 
 function setup()
-    local pos = vec2(WIDTH/2, HEIGHT/2)
-    local steer = vec2(0,0)
-    local speed = 400 -- pixels per second
+    pos = vec2(WIDTH/2, HEIGHT/2)
+    steer = vec2(0,0)
+    speed = 400 -- pixels per second
         
-    local controller = VirtualStick {
+    controller = VirtualStick {
         moved = function(v) steer = v end,
         released = function(v) steer = vec2(0,0) end
     }
     
     controller:activate()
-            
-    function draw()
-        background(131, 228, 73, 255)
-        pos = pos + steer*speed*DeltaTime
-        sprite("Planet Cute:Character Boy", pos.x, pos.y)
-        controller:draw()
-    end
+end
+
+function draw()
+    background(131, 228, 73, 255)
+    
+    pos = pos + steer*speed*DeltaTime
+    
+    sprite("Planet Cute:Character Boy", pos.x, pos.y)
+    controller:draw()
 end
